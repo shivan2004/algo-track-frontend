7ed35a9c9d8700c9875265f1ec14459703698c2b
Shivan Samala <112183968+shivan2004@users.noreply.github.com>
2025-05-17 09:26:15 +0530

    added API url instead of loaclhost to google and github buttons

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:8080';
-// const API_URL = process.env.REACT_APP_API_URL;
 const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
